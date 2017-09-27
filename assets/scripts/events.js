9b7b33ee84e67f5a46991e304d994046a1ad9b28
colekins <colekins@gmail.com>
2017-09-27 14:48:33 -0400

    Update style elements
    Hide Sign Up and Change Password forms until clicked.

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -35,11 +35,16 @@ const onSignOut = function (event) {
     .catch(ui.signOutFail)
 }
 
+const unhide = function () {
+  $(this.parentElement.parentElement.children[1]).toggleClass('hidden unhidden')
+  console.log(this.parentElement.parentElement.children[1])
+}
 const addHandlers = function () {
   $('#sign-up').on('submit', onSignUp)
   $('#sign-in').on('submit', onSignIn)
   $('#change-password').on('submit', onChangePassword)
   $('#sign-out').on('submit', onSignOut)
+  $('.toggle').on('click', unhide)
 }
 
 module.exports = {
